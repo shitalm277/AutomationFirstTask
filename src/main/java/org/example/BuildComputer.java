ba8038c3b71c603fc37c88537a7182041ff02db7
shitalm277 <shitalmehta277@gmail.com>
2021-11-27 23:02:43 +0000

    testngpractice

--- src/main/java/org/example/BuildComputer.java
+++ src/main/java/org/example/BuildComputer.java
@@ -6,13 +6,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BuildComputer {
     static WebDriver driver;
-    public static void main(String[]args){
-        System.setProperty("webdriver.chrome.driver","src/test/Driver/chromedriver.exe");
-        driver = new ChromeDriver();//open the browser
-        driver.manage().window().maximize();//open the browser in full screen
-        driver.get("https://demo.nopcommerce.com/");//it will take you to this website
-        driver.findElement(By.xpath("//div[@class='header-menu']/ul[1]/li[1]/a")).click();//clicks on the Computer menu
-        driver.findElement(By.xpath("//ul[@class='sublist']/li[1]/a")).click();//clicks on Desktops menu
-        driver.findElement(By.xpath("//div[@class='item-grid']/div[1]/div[1]/div[2]/h2/a")).click();//clicks on Build your own computer
+
+    public BuildComputer() {
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/test/Driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.xpath("//div[@class='header-menu']/ul[1]/li[1]/a")).click();
+        driver.findElement(By.xpath("//ul[@class='sublist']/li[1]/a")).click();
+        driver.findElement(By.xpath("//div[@class='item-grid']/div[1]/div[1]/div[2]/h2/a")).click();
+        String actualText = driver.findElement(By.xpath("//div[@class='product-name']/h1")).getText();
+        System.out.println(actualText);
+        driver.close();
     }
 }
--- src/main/java/org/example/Details.java
+++ src/main/java/org/example/Details.java
@@ -7,16 +7,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Details {
     static WebDriver driver;
 
+    public Details() {
+    }
+
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","src/test/Driver/chromedriver.exe");
-        driver = new ChromeDriver(); // open the browser
-        driver.manage().window().maximize(); //open the browser in full screen
-        driver.get("https://demo.nopcommerce.com/"); //the website will be opened
-        driver.findElement(By.xpath("//div[@class='news-items']/div[2]/div[3]/a")).click();// details link will be opened
-        //in the title text box it will type as Maven homework automatically. no need to type.
-        driver.findElement(By.id("AddNewComment_CommentTitle")).sendKeys("Maven Project homework");
-        //in the comment text box it will type the comments automatically. no need to type.
-        driver.findElement(By.id("AddNewComment_CommentText")).sendKeys("This is the first homework of Maven Project.");
-        driver.findElement(By.xpath("//div[@class='buttons']/button[1]")).click(); //it will click on the New Comment button
+        System.setProperty("webdriver.chrome.driver", "src/test/Driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.xpath("//div[@class='news-items']/div[2]/div[3]/a")).click();
+        driver.findElement(By.id("AddNewComment_CommentTitle")).sendKeys(new CharSequence[]{"Maven Project homework"});
+        driver.findElement(By.id("AddNewComment_CommentText")).sendKeys(new CharSequence[]{"This is the first homework of Maven Project."});
+        driver.findElement(By.xpath("//div[@class='buttons']/button[1]")).click();
+        String actualText = driver.findElement(By.xpath("//div[@class='result']")).getText();
+        System.out.println(actualText);
+        driver.close();
     }
 }
